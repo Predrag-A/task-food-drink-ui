7930f06c0579e1f56b493c16152989bca012483f
Predrag Antanasijevic - Praksa <pran0701@enjoying.rs>
2019-04-19 15:57:18 +0200

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+//------------------------------------------------------------------------------
+// Dependencies
+//------------------------------------------------------------------------------
+const gulp = require('gulp');
+
+const sass = require('gulp-sass'), 
+      sassLint = require('gulp-sass-lint'), 
+      imagemin = require('gulp-imagemin'), 
+      autoprefixer = require('gulp-autoprefixer');
+
+//------------------------------------------------------------------------------
+// Compilation
+//------------------------------------------------------------------------------
+
+// Copy HTML
+gulp.task('compile-html', function(){
+    return gulp
+        .src('src/**/*.html')
+        .pipe(gulp.dest('dist'));
+})
+
+// Lint and Compile SASS
+gulp.task('compile-sass', function(){
+    return gulp
+        .src('src/scss/*.scss')
+        .pipe(sassLint())
+        .pipe(sassLint.format())
+        .pipe(sassLint.failOnError())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('dist/css'));
+})
+
+// Compile Images
+gulp.task('compile-img', function(){
+    return gulp
+        .src('src/assets/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/assets/images'));
+})
+
+// Compile all
+gulp.task('compile', gulp.parallel('compile-html', 
+                                   'compile-sass', 
+                                   'compile-img'))
+
+//------------------------------------------------------------------------------
+// Watching
+//------------------------------------------------------------------------------
+
+// Group watchers together
+const watchGroup = function(){
+    gulp.watch('src/**/*.html', gulp.series('compile-html'));
+    gulp.watch('src/scss/**/*.scss', gulp.series('compile-sass'));
+    gulp.watch('src/assets/images/*', gulp.series('compile-img'));
+}
+
+// Run compiler
+gulp.task('watch', gulp.series('compile', watchGroup));
